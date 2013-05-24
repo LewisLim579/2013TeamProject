de7286fa122980df97152ae4d98da5eb1f22be7e
kimkiwoong <llagall2@naver.com>
2013-05-25 00:47:25 +0900

    test

--- PtsApp/gen/com/example/ptsapp/R.java
+++ PtsApp/gen/com/example/ptsapp/R.java
@@ -49,16 +49,17 @@ public final class R {
         public static final int oi_2=0x7f020019;
         public static final int oititle=0x7f02001a;
         public static final int rbtn=0x7f02001b;
-        public static final int sese=0x7f02001c;
-        public static final int smile=0x7f02001d;
-        public static final int sttt=0x7f02001e;
-        public static final int usuallyback=0x7f02001f;
-        public static final int warning=0x7f020020;
-        public static final int warning2=0x7f020021;
-        public static final int whatbbm=0x7f020022;
-        public static final int whatbmi2=0x7f020023;
-        public static final int whatde=0x7f020024;
-        public static final int youbmi=0x7f020025;
+        public static final int schedule=0x7f02001c;
+        public static final int sese=0x7f02001d;
+        public static final int smile=0x7f02001e;
+        public static final int sttt=0x7f02001f;
+        public static final int usuallyback=0x7f020020;
+        public static final int warning=0x7f020021;
+        public static final int warning2=0x7f020022;
+        public static final int whatbbm=0x7f020023;
+        public static final int whatbmi2=0x7f020024;
+        public static final int whatde=0x7f020025;
+        public static final int youbmi=0x7f020026;
     }
     public static final class id {
         public static final int TextView01=0x7f080001;
@@ -90,7 +91,8 @@ public final class R {
         public static final int distinguish=0x7f030005;
         public static final int loading=0x7f030006;
         public static final int menu=0x7f030007;
-        public static final int starc=0x7f030008;
+        public static final int schedule=0x7f030008;
+        public static final int starc=0x7f030009;
     }
     public static final class menu {
         public static final int main=0x7f070000;
