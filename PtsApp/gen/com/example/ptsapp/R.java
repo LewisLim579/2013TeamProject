3da3e803c47d8b0b7999ef21187a205717825222
LewisLim579 <samo57980@gmail.com>
2013-05-25 02:22:05 +0900

    midnight

--- PtsApp/gen/com/example/ptsapp/R.java
+++ PtsApp/gen/com/example/ptsapp/R.java
@@ -63,23 +63,24 @@ public final class R {
     }
     public static final class id {
         public static final int TextView01=0x7f080001;
-        public static final int action_settings=0x7f080012;
+        public static final int action_settings=0x7f080013;
         public static final int bbt1=0x7f080009;
         public static final int bbt2=0x7f08000a;
-        public static final int btn1=0x7f08000c;
-        public static final int btn2=0x7f08000d;
-        public static final int btn3=0x7f08000e;
+        public static final int btn1=0x7f08000d;
+        public static final int btn2=0x7f08000e;
+        public static final int btn3=0x7f08000f;
         public static final int height=0x7f080002;
         public static final int imageView1=0x7f080000;
         public static final int imageView2=0x7f080007;
         public static final int imageView3=0x7f080008;
-        public static final int jungb=0x7f080011;
-        public static final int nab=0x7f08000f;
-        public static final int oib=0x7f080010;
+        public static final int jungb=0x7f080012;
+        public static final int nab=0x7f080010;
+        public static final int oib=0x7f080011;
         public static final int rbtn=0x7f080004;
         public static final int rebmi=0x7f080005;
+        public static final int relativeLayout1=0x7f08000b;
         public static final int remark=0x7f080006;
-        public static final int textView1=0x7f08000b;
+        public static final int textView1=0x7f08000c;
         public static final int weight=0x7f080003;
     }
     public static final class layout {
