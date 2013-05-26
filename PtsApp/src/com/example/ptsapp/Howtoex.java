14272e8ec2a6e7837e497561c6e260341c4ec682
LewisLim579 <samo57980@gmail.com>
2013-05-26 22:17:47 +0900

    sunday!!!

--- PtsApp/src/com/example/ptsapp/Howtoex.java
+++ PtsApp/src/com/example/ptsapp/Howtoex.java
@@ -1,7 +1,10 @@
 package com.example.ptsapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Howtoex extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,21 @@ public class Howtoex extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.howtoex);
+        ImageButton sang = (ImageButton) findViewById(R.id.sangbtn);
+        ImageButton ha = (ImageButton) findViewById(R.id.habtn);
+        
+        sang.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Howtoex.this, Sangche.class);
+				startActivity(intent);
+			}
+		});
+        
+        ha.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Howtoex.this, Hache.class);
+				startActivity(intent);
+			}
+		});
     }
 }
