91df8a754cecfdbb2228fa6119ec8e0b0e7580a7
LewisLim579 <samo57980@gmail.com>
2013-05-28 02:46:32 +0900

    how to exercise_detail

--- PtsApp/src/com/example/ptsapp/Hache.java
+++ PtsApp/src/com/example/ptsapp/Hache.java
@@ -1,7 +1,10 @@
 package com.example.ptsapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Hache extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,28 @@ public class Hache extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hache);
+        ImageButton h1 = (ImageButton) findViewById(R.id.h1);
+		ImageButton h2 = (ImageButton) findViewById(R.id.h2);
+		ImageButton h3 = (ImageButton) findViewById(R.id.h3);
+
+		h1.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Hache.this, Ha1.class);
+				startActivity(intent);
+			}
+		});
+		h2.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Hache.this, Ha2.class);
+				startActivity(intent);
+			}
+		});
+		h3.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Hache.this, Ha3.class);
+				startActivity(intent);
+			}
+		});
+
     }
 }
--- PtsApp/src/com/example/ptsapp/Sangche.java
+++ PtsApp/src/com/example/ptsapp/Sangche.java
@@ -1,7 +1,10 @@
 package com.example.ptsapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Sangche extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,40 @@ public class Sangche extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sangche);
+        ImageButton s1 = (ImageButton) findViewById(R.id.sa1);
+		ImageButton s2 = (ImageButton) findViewById(R.id.sa2);
+		ImageButton s3 = (ImageButton) findViewById(R.id.sa3);
+		ImageButton s4 = (ImageButton) findViewById(R.id.sa4);
+		ImageButton s5 = (ImageButton) findViewById(R.id.sa5);
+		s1.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Sangche.this, Sa1.class);
+				startActivity(intent);
+			}
+		});
+		s2.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Sangche.this, Sa2.class);
+				startActivity(intent);
+			}
+		});
+		s3.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Sangche.this, Sa3.class);
+				startActivity(intent);
+			}
+		});
+		s4.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Sangche.this, Sa4.class);
+				startActivity(intent);
+			}
+		});
+		s5.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Sangche.this, Sa5.class);
+				startActivity(intent);
+			}
+		});
     }
 }
