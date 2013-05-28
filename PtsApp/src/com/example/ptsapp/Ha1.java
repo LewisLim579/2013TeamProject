21814abcd199e59a8d847c9ff4ccd0b278f4c5ff
LewisLim579 <samo57980@gmail.com>
2013-05-28 16:16:34 +0900

    complete how to exercise exception media file

--- PtsApp/src/com/example/ptsapp/Ha1.java
+++ PtsApp/src/com/example/ptsapp/Ha1.java
@@ -1,7 +1,10 @@
 package com.example.ptsapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Ha1 extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,19 @@ public class Ha1 extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ha1);
+        ImageButton s1 = (ImageButton) findViewById(R.id.imageButton1);
+		s1.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Ha1.this, How15.class);
+				startActivity(intent);
+			}
+		});
+		ImageButton s2 = (ImageButton) findViewById(R.id.imageButton2);
+		s2.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Ha1.this, How16.class);
+				startActivity(intent);
+			}
+		});
     }
 }
