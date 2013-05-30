5556fa40df691a46b58df2d95e77d849c7b93195
LewisLim579 <samo57980@gmail.com>
2013-05-31 00:29:11 +0900

    apply pinch to zoom

--- PtsApp/src/com/example/ptsapp/How1.java
+++ PtsApp/src/com/example/ptsapp/How1.java
@@ -1,11 +1,4 @@
-/***
- * Excerpted from "Hello, Android! 3e",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/eband3 for more book information.
-***/
+
 package com.example.ptsapp;
 
 import android.app.Activity;
