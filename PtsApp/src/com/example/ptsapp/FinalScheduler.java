df9de450779f6db49d521a4211eb5058bfa090d0
LewisLim579 <samo57980@gmail.com>
2013-06-13 00:02:31 +0900

    Complete today exercise and button

--- PtsApp/src/com/example/ptsapp/FinalScheduler.java
+++ PtsApp/src/com/example/ptsapp/FinalScheduler.java
@@ -1,7 +1,10 @@
 package com.example.ptsapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class FinalScheduler extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,13 @@ public class FinalScheduler extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fschedule);
+		ImageButton b1 = (ImageButton) findViewById(R.id.todayexercise);
+		
+		b1.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(FinalScheduler.this, TodayExercise.class);
+				startActivity(intent);
+			}
+		});
     }
 }
